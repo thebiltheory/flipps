f2751db583f41756c7ed3d0415701c02c03f07ea
Bil Benhamou <nbenhamou@mafcarrefour.com>
2019-05-23 14:33:41 +0400

    feat: init flips

---
+++ src/flips.js
@@ -0,0 +1,20 @@
+import rtlcss from "rtlcss";
+
+// https://github.com/thysultan/stylis.js#plugins
+const STYLIS_CONTEXTS = {
+  POST_PROCESS: -2,
+  PREPARATION: -1,
+  NEWLINE: 0,
+  PROPERTY: 1,
+  SELECTOR_BLOCK: 2,
+  AT_RULE: 3,
+};
+
+export type StylisContextType = $Values<typeof STYLIS_CONTEXTS>;
+export const STYLIS_PROPERTY_CONTEXT = STYLIS_CONTEXTS.PROPERTY;
+
+export default (context: StylisContextType, content: string): ?string => {
+  if (context === STYLIS_PROPERTY_CONTEXT) {
+    return rtlcss.process(content);
+  }
+};
---
+++ src/flips.test.js
@@ -0,0 +1,48 @@
+import stylisRtlPlugin, { STYLIS_PROPERTY_CONTEXT } from "./flips";
+
+describe("Stylis RTL Plugin", () => {
+  it("converts LTR to RTL", () => {
+    expect(
+      stylisRtlPlugin(STYLIS_PROPERTY_CONTEXT, "padding-left: 2px;"),
+    ).toEqual("padding-right: 2px;");
+    expect(
+      stylisRtlPlugin(STYLIS_PROPERTY_CONTEXT, "margin: 0 1px 0 2px;"),
+    ).toEqual("margin: 0 2px 0 1px;");
+  });
+
+  it("allows you to replace a value via inline comments", () => {
+    expect(
+      stylisRtlPlugin(STYLIS_PROPERTY_CONTEXT, "font-size: 16px/*rtl:14px*/;"),
+    ).toEqual("font-size: 14px;");
+  });
+
+  it("allows you to skip rules via comments", () => {
+    const input = `
+      margin: 0 2px 0 1px;
+      /*rtl:begin:ignore*/
+      text-align:left;
+      /*rtl:end:ignore*/
+      /* just a regular comment */
+      margin: 0 2px 0 1px;
+    `;
+
+    const output = `
+      margin: 0 1px 0 2px;
+      text-align:left;
+      /* just a regular comment */
+      margin: 0 1px 0 2px;
+    `;
+    expect(stylisRtlPlugin(STYLIS_PROPERTY_CONTEXT, input)).toEqual(output);
+  });
+
+  it("allows you to prepend a value via comments & variables", () => {
+    const arabicFont = "Droid Arabic Kufi";
+
+    expect(
+      stylisRtlPlugin(
+        STYLIS_PROPERTY_CONTEXT,
+        `font-family: "Droid Sans", sans-serif/*rtl:prepend:"${arabicFont}",*/;`,
+      ),
+    ).toEqual('font-family: "Droid Arabic Kufi","Droid Sans", sans-serif;');
+  });
+});
